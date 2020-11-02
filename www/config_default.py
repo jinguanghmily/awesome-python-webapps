f373f747be5a53471c50b4889402a72bc528c9a3
jinguanghmily <jin_guang@qq.com>
2020-11-02 13:30:07 +0900

    day5

--- www/config_default.py
+++ www/config_default.py
@@ -4,10 +4,10 @@
 configs = {
     'debug': True,
     'db':{
-        'host': '127.0.01',
+        'host': '127.0.0.1',
         'port': 3306,
-        'user': 'www',
-        'password': 'www',
+        'user': 'root',
+        'password': 'root',
         'db': 'awesome'
     },
     'sesion':{
